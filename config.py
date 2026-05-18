43a6c8fbdd81481c085d39b03051c38e32c70c5c
rohit3607 <rohitplayer87089@gmail.com>
2026-05-19 04:59:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ ADMINS = [
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "mongodb+srv://rohitplayer87089:rohit870@cluster0.4wt927p.mongodb.net/?retryWrites=true&w=majority") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+DB_NAME = environ.get("DB_NAME", "Req")
 
 # If this is True Then Bot Accept New Join Request 
 NEW_REQ_MODE = bool(environ.get('NEW_REQ_MODE', True))
